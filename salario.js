787d814a120795c0b42dcfdfa30d0a4dbe721d43
nvitoria200 <168544916+nvitoria200@users.noreply.github.com>
2025-02-28 08:49:32 -0300

    Add files via upload

---
+++ salario.js
@@ -0,0 +1,14 @@
+let salario = 3300;
+let result = 0;
+if(salario >= 3500){
+    (result = salario-500)}
+    else
+    if(salario >= 3500){ 
+        result = salario-300}
+        else
+        if(salario>=2500){
+            result= salario-150 }
+    else
+    if(salario<=2499){
+        result = "ISENTO"}
+console.log("O valor final de salário com desconto é de:" + result)
